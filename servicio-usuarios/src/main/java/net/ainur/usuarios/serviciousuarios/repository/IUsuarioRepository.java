b839620b70ddb1eeccb996a3fcee812b7ebc23de
Danny <live626@gmail.com>
2022-03-07 16:51:17 -0400

    feat: crud servicio-usuarios

--- servicio-usuarios/src/main/java/net/ainur/usuarios/serviciousuarios/repository/IUsuarioRepository.java
+++ servicio-usuarios/src/main/java/net/ainur/usuarios/serviciousuarios/repository/IUsuarioRepository.java
@@ -1,9 +1,11 @@
 package net.ainur.usuarios.serviciousuarios.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import net.ainur.usuarios.serviciousuarios.models.Usuario;
 
+@RepositoryRestResource(path = "usuarios")
 public interface IUsuarioRepository extends JpaRepository<Usuario, Long> {
     
     // este es un query method
